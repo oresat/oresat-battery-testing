10b9049eb7aea08d298ab37508725f2c6c4a5ab9
Lev <lev8@pdx.edu>
2023-03-16 11:00:12 -0700

    Added csv file writer, and timestamper.

--- software/playground.py
+++ software/playground.py
@@ -12,3 +12,38 @@ bb = BatteryBank(blj, 3)
 bb.measure()
 print(bb.cellVolts)
 print(bb.cellTemps)
+
+#blame Lev below this line
+from datetime import datetime
+import csv
+
+#function takes in timestamp and voltage, writes values to csv file, "a" appends whereas "w" clears file first
+def write_to_csv(date, voltage):
+    with open("timestamps.csv", "a", newline = "") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([date, voltage])
+        #writer.writerow("\n")
+
+#function clears csv file, just scaffolding for now, "w+" truncates the file
+def clear_csv(): 
+    with open("timestamps.csv", "w+", newline = "") as csv_file:
+        pass
+
+#function makes a timestamp
+def stamper():
+    dt = datetime.now()
+    iso_date = dt.isoformat()
+    return iso_date
+
+check = input("Do you want to clear the csv file, good sir? \nY/n: ")
+if (check.upper == 'Y'):
+    clear_csv()
+
+for x in range(5):
+    #time.sleep(1)
+    voltage = bb.cellVolts #don't actually know what bb.cellVolts is measuring (gives four voltage values?)
+    date = stamper()
+    print(str(voltage))
+    print(str(date))
+    write_to_csv(date, voltage)
+    #/home/lev/oresat-battery-testing/timestamps.csv
